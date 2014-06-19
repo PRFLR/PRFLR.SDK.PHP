ea255d82f28ccf932c084ec2c930c61c0053749b
A.E. <thaheless@gmail.com>
2014-06-20 01:53:55 +0700

    PRFLR init fails catch

--- PRFLRLogRoute.php
+++ PRFLRLogRoute.php
@@ -5,40 +5,52 @@
  * @note following options should be enabled in config
  *     Yii::getLogger()->autoFlush = 1;
  *     Yii::getLogger()->autoDump = 1;
- * 
+ *
  * @author     Alexey Spiridonov <forspall@gmail.com>
  */
-class PRFLRLogRoute extends CLogRoute {
-
-    public $source;
+class PRFLRLogRoute extends CLogRoute
+{
     public $apikey;
+    public $source;
+    public $category = 'nyrby_ss_v3';
+    public $active = false;
+
 
     /**
      * @brief init log router
      */
-    public function init() {
+    public function init()
+    {
         parent::init();
-        include_once(__DIR__ . '/prflr.php');
-        PRFLR::init($this->source, $this->apikey);
+        if ($this->active) {
+            include_once(__DIR__ . '/prflr.php');
+            try {
+                PRFLR::init($this->source, $this->apikey);
+            } catch (Exception $e) {
+                $this->active = false;
+                Yii::log('Unnable to init PRFLR: ' . $e->getMessage(), CLogger::LEVEL_ERROR, 'PRFLRLogRoute.init');
+            }
+        }
     }
 
     /**
      * Stores log messages 
      * @param array $logs list of log messages
      */
-    protected function processLogs($logs) {
+    protected function processLogs($logs)
+    {
         // work only with first element
         $_log = $logs[0];
         // profile log message have begin: and end: prefixes
-        if ($_log[1] == 'profile') {
+        if ($_log[1] == 'profile' && $this->active) {
             if ($_log[0][0] == 'b') {
                 // begin
                 PRFLR::Begin(substr($_log[0], 6, strlen($_log[0])));
             } else {
-                //end
-                PRFLR::End(substr($_log[0], 4, strlen($_log[0])), $_log[2]);
+                // base category if category not set 
+                $category = !empty($_log[2]) && $_log[2] != 'application' ? $_log[2] : $this->category;
+                PRFLR::End(substr($_log[0], 4, strlen($_log[0])), $category);
             }
         }
     }
-
 }
